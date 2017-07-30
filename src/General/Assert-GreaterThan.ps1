1def6b8383e18ffd669a4791b5232fc490a861a5
nohwnd <me@jakubjares.com>
2017-07-30 23:19:12 +0200

    Add Assert-GreaterThan and Assert-GreaterThanOrEqual

---
+++ src/General/Assert-GreaterThan.ps1
@@ -0,0 +1,18 @@
+function Assert-GreaterThan {
+    param (
+        [Parameter(ValueFromPipeline=$true)]
+        $Actual, 
+        [Parameter(Position=0)]
+        $Expected,
+        [String]$Message
+    )
+
+    $Actual = Collect-Input -ParameterInput $Actual -PipelineInput $local:Input
+    if ($Expected -ge $Actual) 
+    { 
+        $Message = Get-AssertionMessage -Expected $Expected -Actual $Actual -Message $Message -DefaultMessage "Expected <actualType> '<actual>' to be greater than <expectedType> '<expected>', but it was not."
+        throw [Assertions.AssertionException]$Message
+    }
+
+    $Actual
+}
---
+++ src/General/Assert-GreaterThanOrEqual.ps1
@@ -0,0 +1,18 @@
+function Assert-GreaterThanOrEqual {
+    param (
+        [Parameter(ValueFromPipeline=$true)]
+        $Actual, 
+        [Parameter(Position=0)]
+        $Expected,
+        [String]$Message
+    )
+
+    $Actual = Collect-Input -ParameterInput $Actual -PipelineInput $local:Input
+    if ($Expected -gt $Actual) 
+    { 
+        $Message = Get-AssertionMessage -Expected $Expected -Actual $Actual -Message $Message -DefaultMessage "Expected <actualType> '<actual>' to be greater than or equal to <expectedType> '<expected>', but it was not."
+        throw [Assertions.AssertionException]$Message
+    }
+
+    $Actual
+}
