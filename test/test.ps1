95b8fd9af3712aaf6d6c1f6887689a68736ded1c
nohwnd <me@jakubjares.com>
2017-07-30 13:32:49 +0200

    Import TypeClass module through Axioms

--- test/test.ps1
+++ test/test.ps1
@@ -4,16 +4,16 @@ pushd $PSScriptRoot
 
 get-module pester, assert, axioms, testHelpers | Remove-Module -force
 
-Import-Module Pester
+# import the tested module
 Import-Module .\..\Assert.psm1
+
+# import modules and utilities for testing 
+Import-Module Pester
 Import-Module .\TestHelpers.psm1
 Import-Module .\Axioms\Axioms.psm1 -WarningAction SilentlyContinue
-Import-Module .\..\src\TypeClass\TypeClass.psm1
 
-
-Write-host (New-Dictionary @{name="jakub"})
 Get-Date
 
-Invoke-Pester (Resolve-Path $Path)
+Invoke-Pester (Resolve-Path $Path) -Show Summary, Fails
 
 popd
