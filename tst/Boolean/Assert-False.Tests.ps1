a97cfcaff1a5c4f4e5a4228d51d7529b68cb8818
Irwin Strachan <irwins@users.noreply.github.com>
2017-08-02 09:09:47 -0400

    Update Assert-False.Tests.ps1
    Edited some Testcase description to give it more meaning...

--- tst/Boolean/Assert-False.Tests.ps1
+++ tst/Boolean/Assert-False.Tests.ps1
@@ -1,13 +1,13 @@
 Describe "Assert-False" {
-        It "Passes when given `$false" {
+        It "Passes when cmdlet Assert-False is given `$false" {
             $false | Assert-False
         }
 
-        It "Passes when given falsy" -TestCases @(
-            @{ Actual = 0 }
-            @{ Actual = "" }
-            @{ Actual = $null }
-            @{ Actual = @() }
+        It "Passes when cmdlet Assert-False is given an actual value of <Value>" -TestCases @(
+            @{ Actual = 0 ;Value = '0'}
+            @{ Actual = ""; Value = 'Empty String ""' }
+            @{ Actual = $null; Value = '$null' }
+            @{ Actual = @(); Value = 'Empty Collection @()' }
         ) {
             param($Actual)
             Assert-False -Actual $Actual
@@ -23,7 +23,7 @@ Describe "Assert-False" {
     }
 
     Context "Validate messages" {
-        It "Given value '<expected>' that is not `$false it returns expected message '<message>'" -TestCases @(
+        It "Given value '<Actual>' that is not `$false it returns expected message '<message>'" -TestCases @(
             @{ Actual = $true ; Message = "Expected bool '`$true' to be bool '`$false' or falsy value 0, """", `$null, @()."},
             @{ Actual = 10 ; Message = "Expected int '10' to be bool '`$false' or falsy value 0, """", `$null, @()."}
         ) { 
@@ -33,7 +33,7 @@ Describe "Assert-False" {
         }
     }
 
-    It "Returns the value on output" {
+    It 'Returns the value on output' {
         $expected = $false
         $expected | Assert-False | Verify-Equal $expected
     }
